3c097d02ea9e7fa6bc3305d82ed6734bc71cde29
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2025-03-18 23:44:20 +0100

    fixing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { KezdolapComponent } from './kezdolap/kezdolap.component';
 import { EredmenyComponent } from './eredmeny/eredmeny.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     {'path': '', component: KezdolapComponent},
     {'path': 'eredmeny', component: EredmenyComponent},
     {'path': '**', redirectTo: '', pathMatch: 'full'}
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
